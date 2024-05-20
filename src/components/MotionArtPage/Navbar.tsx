014d76ef2deebabe5191b2bab14bbe5c5fd57e0a
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-05-20 18:41:47 +0530

    add: button hover interaction

--- src/components/MotionArtPage/Navbar.tsx
+++ src/components/MotionArtPage/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="font-outfit">
       <div className="flex justify-between items-center px-3 h-20 pt-5 max-w-[1280px] mx-auto">
         <img src={maLogo} alt="MotionArt Effect" />
-        <button className="px-9 py-4 rounded-md bg-white max-lg:hidden">Purchase Now</button>
+        <button className="px-9 py-4 rounded-md bg-white max-lg:hidden hover:bg-transparent border-2 border-white hover:text-white pointer-events-auto">Purchase Now</button>
       </div>
     </nav>
   );
